889a18196a10e62954ff68f840944aaa76049ef9
Abhishek Kumar <abhishekkunkal3@gmail.com>
2025-12-05 21:45:03 +0530

    chore(security): Move JWT security functions to security.py
    - Moved generate_token and decode_token functions to security.py
    - Ensured proper modularity of JWT-related logic

---
+++ crypto-api/core/security.py
@@ -0,0 +1,20 @@
+import jwt
+from datetime import datetime, timedelta
+
+SECRET_KEY = 'your_secret_key'
+
+def generate_token(user_id):
+    payload = {
+        'exp': datetime.utcnow() + timedelta(days=1),
+        'iat': datetime.utcnow(),
+        'sub': user_id
+    }
+    return jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+def decode_token(token):
+    try:
+        return jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+    except jwt.ExpiredSignatureError:
+        raise Exception("Token has expired")
+    except jwt.InvalidTokenError:
+        raise Exception("Invalid token")
